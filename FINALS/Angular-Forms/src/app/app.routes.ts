252f764d1bdde40f61d96475cf7daf722949accb
Angelene Guiao <angeleneguiao04@gmail.com>
2026-02-26 02:40:06 +0800

    Delete FINALS/Angular-Forms/src directory

--- FINALS/Angular-Forms/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register';
-
-export const routes: Routes = [
-  { path: '', component: RegisterComponent }
-];
